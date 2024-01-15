0d2bf73af88854baba13d5bf3e6b2abd9bc9a629
HahyunKang <ganghahyun06@gmail.com>
2024-01-15 21:06:02 +0900

    성능 개선

--- portfolio/src/main/kotlin/com/hahyeon/portfolio/domain/repository/ProjectSkillRepository.kt
+++ portfolio/src/main/kotlin/com/hahyeon/portfolio/domain/repository/ProjectSkillRepository.kt
@@ -7,6 +7,6 @@ import java.util.Optional
 interface ProjectSkillRepository : JpaRepository<ProjectSkill, Long> {
 
     //select * from project_skill where project_id = :projectId and skill_id =: skillId
-    fun findByProjectIdAndSkillId(projectId: Long, skillId: Long) : Optional<Project>
+    fun findByProjectIdAndSkillId(projectId: Long, skillId: Long) : Optional<ProjectSkill>
     
 }
